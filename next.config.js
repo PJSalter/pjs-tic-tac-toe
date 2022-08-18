2f894430ad607f9ccf05a272ff6b59c0347e2d68
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-08-18 22:27:17 +0100

    commented out css module rules that I may initially use later on

--- next.config.js
+++ next.config.js
@@ -2,6 +2,24 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.css$/,
+  //       include: path.join(__dirname, "src/components"),
+  //       use: [
+  //         "style-loader",
+  //         {
+  //           loader: "typings-for-css-modules-loader",
+  //           options: {
+  //             modules: true,
+  //             namedExport: true,
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
