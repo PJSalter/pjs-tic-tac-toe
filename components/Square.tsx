f78ace52674ef99112398799740f3e9de9dab5b5
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-08-19 15:34:32 +0100

    defining the types for each prop being used

--- components/Square.tsx
+++ components/Square.tsx
@@ -1,4 +1,16 @@
-function Square() {
+// defining the props
+function Square({
+  value,
+  onClick,
+  winner,
+}: {
+  winner: string | null;
+  value: string | null;
+  onClick: () => void;
+}) {
+  if (!value) {
+    return <button disabled={Boolean(winner)} />;
+  }
   return <button>This is a square</button>;
 }
 
