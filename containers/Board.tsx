17c3237bf252d6afd9688980cce7ab52a7a7e6c6
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-08-19 14:34:01 +0100

    setting a use state for winner

--- containers/Board.tsx
+++ containers/Board.tsx
@@ -7,9 +7,11 @@ function Board() {
   const [currentPlayer, setCurrentPlayer] = useState<"X" | "0">(
     Math.round(Math.random() * 1) === 1 ? "X" : "0"
   );
+  // set the winner at an origin value of null.
+  const [winner, setWinner] = useState(null);
   return (
     <div>
-      <p>Hey {currentPlayer}, it\s your turn</p>
+      <p>Hey {currentPlayer}, it is your turn</p>
       This is the board
     </div>
   );
