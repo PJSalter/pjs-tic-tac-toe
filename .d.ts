09632166727f553312e86bf86c01c8cfe62baa98
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-08-31 23:15:38 +0100

    removed file because it is not needed anymore

--- .d.ts
+++
@@ -1,7 +0,0 @@
-declare module "styled-transition-group" {
-  import { transition } from "styled-transition-group";
-  import type { StyledInterface } from "styled-components/macro";
-
-  const transition: StyledInterface<CSSTransition>;
-  export default transition;
-}
