60a5d3b5496c3cf17ff5c6f17bb09fb8e73e9cf0
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-08-31 23:08:49 +0100

    module to help recognise transition group for typescript

--- .d.ts
+++ .d.ts
@@ -1,7 +1,7 @@
-// declare module "styled-transition-group" {
-//   import { transition } from "styled-transition-group";
-//   import type { StyledInterface } from "styled-components/macro";
+declare module "styled-transition-group" {
+  import { transition } from "styled-transition-group";
+  import type { StyledInterface } from "styled-components/macro";
 
-//   const transition: StyledInterface<CSSTransition>;
-//   export default transition;
-// }
+  const transition: StyledInterface<CSSTransition>;
+  export default transition;
+}
