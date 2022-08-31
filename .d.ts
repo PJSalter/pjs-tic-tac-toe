9db1fca35d201845bcb12cc7c84ab5a21e88931c
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-08-31 23:05:47 +0100

    add module for styled transition group

---
+++ .d.ts
@@ -0,0 +1,7 @@
+// declare module "styled-transition-group" {
+//   import { transition } from "styled-transition-group";
+//   import type { StyledInterface } from "styled-components/macro";
+
+//   const transition: StyledInterface<CSSTransition>;
+//   export default transition;
+// }
